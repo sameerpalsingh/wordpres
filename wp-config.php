4f69086b6054713e5000a152bf5ca1845859cda5
Sameer Singh <sameersingh@copart.com>
2017-06-05 20:01:16 +0530

    change db details

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'dinerinn');
+define('DB_NAME', 'wordpres');
 
 /** MySQL database username */
-define('DB_USER', 'dinerinn');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'dsd831n663M5');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
